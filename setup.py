98fb1ae63dccf336e3df63e23053fccd4ca69075
kwakaba <kwakaba@slis.tsukuba.ac.jp>
2023-06-23 09:15:44 +0900

    delete PantheonRL from setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     version="0.0.1",
     install_requires=[
         "stable-baselines3==1.6.2",
-        "sb3-contrib==1.6.2",
-        "PantheonRL @ git+https://github.com/Stanford-ILIAD/PantheonRL.git@1a591040784511bcf738d2acdec055c597e79180"
+        "sb3-contrib==1.6.2"
     ]
 )
